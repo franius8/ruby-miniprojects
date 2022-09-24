7594c8b397d60c4f7d28696bd1bb82b06bbcf43c
franius8 <skawinskifranciszek@gmail.com>
2022-09-24 21:47:02 +0200

    Add stock_picker function
    Stock_picker function calculates the best days to buy and sell stock

---
+++ stock_picker/stock_picker.rb
@@ -0,0 +1,22 @@
+def stock_picker(prices)
+    low_price = prices[0]
+    low_index = 0
+    profit = 0
+    result = [0, 0]
+
+    prices.each_with_index do |price, index|
+        if price < low_price
+            low_price = price
+            low_index = index
+        end
+
+        if price - low_price > profit
+            profit = price - low_price
+            result = [low_index, index]
+        end
+    end
+    print result
+end
+
+prices = [17, 3, 6, 9, 15, 8, 5, 1, 10]
+stock_picker(prices) 
