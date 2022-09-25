bf026201faaa296b4af2c2263d67b3760a433cdf
franius8 <skawinskifranciszek@gmail.com>
2022-09-25 20:17:01 +0200

    Add switch pair and is_next_smaller functions

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,12 +1,21 @@
+def switch_pair (array, number, index)
+    array[index] = array[index - 1]
+    array[index - 1] = number
+    array
+end
+
+def is_next_smaller?(array, number, index)
+    number < array[index - 1]
+end
+
 def bubble_sort (array)
     sorted = false
     until sorted == true
         sorted = true
         array.each_with_index do |number, index|
             unless index == 0
-                if number < array[index - 1]
-                    array[index] = array[index - 1]
-                    array[index - 1] = number
+                if is_next_smaller?(array, number, index)
+                    switch_pair(array, number, index)
                     sorted = false
                 end
             end
@@ -15,4 +24,4 @@ def bubble_sort (array)
     puts array
 end
 
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([4,3,78,2,0,1])
