39b088bae9e2b5733a018553f062ff924abdf282
franius8 <skawinskifranciszek@gmail.com>
2022-09-25 20:03:48 +0200

    Create is_letter? and outside_range? functions

--- caesar-cipher/cipher.rb
+++ caesar-cipher/cipher.rb
@@ -1,11 +1,19 @@
+def is_letter?(number)
+    number.between?(65, 90) || number.between?(97, 122)
+end
+
+def outside_range?(number, char)
+    number > 122 || (char.ord.between?(65, 90) && number > 90)
+end
+
 def caesar_cipher (string, shift)
     array = string.split('')
     final_array = Array.new
-    array.each do |char|
+    array.each do |char| 
         number = char.ord
-        if number.between?(65, 90) || number.between?(97, 122)
+        if is_letter? (number)
             number += shift
-           while number > 122 || (char.ord.between?(65, 90) && number > 90)
+           while outside_range?(number, char)
             number = number - 26
            end
            final_array << number.chr  
