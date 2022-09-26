65edb0fe7ab5ff914645ac70cb41519b2ee22db0
franius8 <skawinskifranciszek@gmail.com>
2022-09-26 17:11:01 +0200

    Add basic play function (only marker selection)

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -3,13 +3,32 @@
 WINNING_COMBINATIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 class Game
-  attr_reader :board
+  attr_reader :board, :player1, :player2
 
   def initialize
     @board = Array.new(10)
     @board[0] = 'not_used'
-    @player1 = Player.new(self, 'x')
-    @player2 = Player.new(self, 'o')
+    @player1 = Player.new(self, 'x', 'Player 1')
+    @player2 = Player.new(self, 'o', 'Player 2')
+    @current_player = 1
+  end
+
+  def play
+    loop do
+      if @current_player == 1
+        place_marker(@player1)
+        @current_player = 2
+      else
+        place_marker(@player2)
+        @current_player = 1
+      end
+    end
+  end
+
+  def place_marker(player)
+    puts "#{player.name} please select position for your marker"
+    position = player.position_selection
+    @board[position] = player.marker
   end
 
   def available_positions
@@ -22,17 +41,22 @@ class Game
     @board[1..10].none?(&:nil?)
   end
 
-  def won?(player1)
-    WINNING_COMBINATIONS.any? { |line| line.all? { |position| @board[position] == player1.marker } }
+  def won?(player)
+    WINNING_COMBINATIONS.any? { |line| line.all? { |position| @board[position] == player.marker } }
+  end
+
+  def draw?
+    self.board_full? && (self.won?(@player1) || self.won?(@player2))
   end
 end
 
 class Player
-  attr_reader :number, :marker
+  attr_reader :number, :marker, :name
 
-  def initialize(game, marker)
+  def initialize(game, marker, name)
     @game = game
     @marker = marker
+    @name = name
   end
 
   def position_selection
@@ -47,6 +71,5 @@ class Player
 end
 
 game = Game.new
-puts game.available_positions
-print game.board
-puts game.won?(game.player1)
+game.play
+
