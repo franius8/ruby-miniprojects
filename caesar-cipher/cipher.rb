ed38f1d3b908974a87047b3823796c88c39476a7
franius8 <skawinskifranciszek@gmail.com>
2022-09-26 10:07:47 +0200

    Style changes

--- caesar-cipher/cipher.rb
+++ caesar-cipher/cipher.rb
@@ -1,33 +1,33 @@
+# frozen_string_literal: true
+
 def is_letter?(number)
-    number.between?(65, 90) || number.between?(97, 122)
+  number.between?(65, 90) || number.between?(97, 122)
 end
 
 def outside_range?(number, char)
-    number > 122 || (char.ord.between?(65, 90) && number > 90)
+  number > 122 || (char.ord.between?(65, 90) && number > 90)
 end
 
-def caesar_cipher (string, shift)
-    array = string.split('')
-    final_array = Array.new
-    array.each do |char| 
-        number = char.ord
-        if is_letter? (number)
-            number += shift
-           while outside_range?(number, char)
-            number = number - 26
-           end
-           final_array << number.chr  
-        else
-            final_array << char 
-        end
+def caesar_cipher(string, shift)
+  array = string.split('')
+  final_array = []
+  array.each do |char|
+    number = char.ord
+    if is_letter?(number)
+      number += shift
+      number -= 26 while outside_range?(number, char)
+      final_array << number.chr
+    else
+      final_array << char
     end
-    puts final_array.join('')
+  end
+  puts final_array.join('')
 end
 
-print "Enter the phrase to encode: "
+print 'Enter the phrase to encode: '
 string = gets.chomp
 
-print "Enter the shift value: "
+print 'Enter the shift value: '
 shift = gets.chomp.to_i
 
 caesar_cipher(string, shift)
