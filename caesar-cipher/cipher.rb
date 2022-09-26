2cd977bcadf5af38e192e507e78574bb27c464a2
franius8 <skawinskifranciszek@gmail.com>
2022-09-26 10:29:19 +0200

    Split main function into separate functions

--- caesar-cipher/cipher.rb
+++ caesar-cipher/cipher.rb
@@ -8,19 +8,29 @@ def outside_range?(number, char)
   number > 122 || (char.ord.between?(65, 90) && number > 90)
 end
 
-def caesar_cipher(string, shift)
-  array = string.split('')
-  final_array = []
-  array.each do |char|
-    number = char.ord
-    if letter?(number)
-      number += shift
-      number -= 26 while outside_range?(number, char)
-      final_array << number.chr
-    else
-      final_array << char
-    end
+def shift_value(number, char, shift)
+  number += shift
+  number -= 26 while outside_range?(number, char)
+  number.chr
+end
+
+def return_shifted_value(char, shift)
+  number = char.ord
+  if letter?(number)
+    shift_value(number, char, shift)
+  else
+    char
   end
+end
+
+def encode_string(string, shift)
+  string.split('').reduce([]) do |final_array, char|
+    final_array << return_shifted_value(char, shift)
+  end
+end
+
+def caesar_cipher(string, shift)
+  final_array = encode_string(string, shift)
   puts final_array.join('')
 end
 
