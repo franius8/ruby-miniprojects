1510e9ffb8c7049de9bb903b1e8f106be7bb0904
franius8 <skawinskifranciszek@gmail.com>
2022-09-26 10:31:43 +0200

    Style optimization

--- sub-strings/sub-strings.rb
+++ sub-strings/sub-strings.rb
@@ -1,24 +1,24 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+# frozen_string_literal: true
 
-def substrings (string, dictionary)
-    main_string = string.downcase
-    result = Hash.new(0)
-    dictionary.each do |word|
-        if main_string.include?(word)
-            result[word] = main_string.scan(word).length
-        end
-    end
-    if result == {}
-        puts "No matches found!"
-    else    
-        puts result
-    end
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+def substrings(string, dictionary)
+  main_string = string.downcase
+  result = Hash.new(0)
+  dictionary.each do |word|
+    result[word] = main_string.scan(word).length if main_string.include?(word)
+  end
+  if result == {}
+    puts 'No matches found!'
+  else
+    puts result
+  end
 end
 
-puts "The dictionary is:"
+puts 'The dictionary is:'
 puts "#{dictionary} \n"
 
-puts "Enter your string: "
+puts 'Enter your string: '
 strings = gets.chomp
 
 substrings(strings, dictionary)
