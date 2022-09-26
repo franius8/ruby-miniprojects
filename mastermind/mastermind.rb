60faefdbe7bcec0631fcfc4700d89b3aa03e7bfd
franius8 <skawinskifranciszek@gmail.com>
2022-09-26 23:25:51 +0200

    Code refactoring

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -1,49 +1,49 @@
 # frozen_string_literal: true
 
 ALLOWED_COLORS = %w[red white yellow blue orange purple].freeze
-ALL_COMBINATIONS = ALLOWED_COLORS.repeated_permutation(4).to_a.select {|array| array.uniq.length == array.length}
+ALL_COMBINATIONS = ALLOWED_COLORS.repeated_permutation(4).to_a.select { |array| array.uniq.length == array.length }
 
 # Methods common to both player and computer rounds
 module CommonElements
-    def collect(name)
-        loop do
-            puts "Enter your #{name} separated by spaces: "
-            @choice = gets.chomp.split(' ')
-            return @choice unless valid? == false
-      
-            puts "#{name.capitalize} contains invalid colors. Enter it again."
-        end
-    end
+  def collect(name)
+    loop do
+      puts "Enter your #{name} separated by spaces: "
+      @choice = gets.chomp.split(' ')
+      return @choice unless valid? == false
 
-    def valid?
-        @choice.all? { |color| ALLOWED_COLORS.include?(color) }
+      puts "#{name.capitalize} contains invalid colors. Enter it again."
     end
+  end
 
-    def check_exactly_correct(guess)
-        result = 0
-        guess.each_with_index { |color, i| result += 1 if color == @code[i] }
-        result
-      end
+  def valid?
+    @choice.all? { |color| ALLOWED_COLORS.include?(color) }
+  end
 
-    def check_included(guess)
-        result = 0
-        guess.each { |color| result += 1 if @code.include?(color) }
-        result
-    end
+  def check_exactly_correct(guess)
+    result = 0
+    guess.each_with_index { |color, i| result += 1 if color == @code[i] }
+    result
+  end
 
-    def won_game
-        puts "#{@name} guessed correctly in #{@turn_number} guesses!"
-        puts "The code was #{@code.join(' ')}."
-    end
+  def check_included(guess)
+    result = 0
+    guess.each { |color| result += 1 if @code.include?(color) }
+    result
+  end
 
-    def lost_game
-        puts "#{@name} did not guess correctly in the allotted time!"
-        puts "The code was #{@code.join(' ')}."
-    end
+  def won_game
+    puts "#{@name} guessed correctly in #{@turn_number} guesses!"
+    puts "The code was #{@code.join(' ')}."
+  end
 
-    def increase_turn
-        @turn_number += 1
-    end
+  def lost_game
+    puts "#{@name} did not guess correctly in the allotted time!"
+    puts "The code was #{@code.join(' ')}."
+  end
+
+  def increase_turn
+    @turn_number += 1
+  end
 end
 
 # Start a game and allow the player to choose whether they want to guess or provide the coe
@@ -82,7 +82,7 @@ class RoundPlayer
   def initialize
     @code = ALLOWED_COLORS.sample(4)
     @turn_number = 1
-    @name = "You"
+    @name = 'You'
   end
 
   def play
@@ -109,47 +109,52 @@ end
 # Play a round with the computer guessing
 class RoundComputer
   include CommonElements
-    def initialize
-        @turn_number = 1
-        @name = "The computer"
-        @code = collect('code')
-    end
+  def initialize
+    @turn_number = 1
+    @name = 'The computer'
+    @code = collect('code')
+  end
 
-    def play
-        loop do
-            guess
-            return won_game if check_exactly_correct(@guess) == 4
-            check_guess
-            return lost_game if @turn_number == 12
-            narrow_possibilities
-        end
-    end
+  def play
+    loop do
+      guess
+      return won_game if check_exactly_correct(@guess) == 4
 
-    def guess
-        return @guess = ALLOWED_COLORS.sample(4) if @guess.nil?
-        loop do
-            new_guess = @possibilities.sample(4)
-            return @guess = new_guess if new_guess != @guess
-        end
-    end
+      check_guess
+      return lost_game if @turn_number == 12
 
-    def check_guess
-        exactly_correct = check_exactly_correct(@guess)
-        included = check_included(@guess) - exactly_correct
-        increase_turn
+      narrow_possibilities
+      puts @possibilities
+      puts '---'
     end
+  end
 
-    def narrow_possibilities
-        if @possibilities.nil? 
-            @possibilities = ALL_COMBINATIONS.select do
-                |n| check_exactly_correct(n) == check_exactly_correct(@guess) && check_included(n) == check_included(@guess)
-            end
-        else
-            @possibilities = @possibilities.select do
-                |n| check_exactly_correct(n) == check_exactly_correct(@guess) && check_included(n) == check_included(@guess)
-            end
-        end
+  def guess
+    return @guess = ALLOWED_COLORS.sample(4) if @guess.nil?
+
+    loop do
+      new_guess = @possibilities.sample(4)
+      return @guess = new_guess if new_guess != @guess
     end
+  end
+
+  def check_guess
+    exactly_correct = check_exactly_correct(@guess)
+    included = check_included(@guess) - exactly_correct
+    increase_turn
+  end
+
+  def narrow_possibilities
+    if @possibilities.nil?
+      @possibilities = ALL_COMBINATIONS.select do |n|
+        check_exactly_correct(n) == check_exactly_correct(@guess) && check_included(n) == check_included(@guess)
+      end
+    else
+      @possibilities = @possibilities.select do |n|
+        check_exactly_correct(n) == check_exactly_correct(@guess) && check_included(n) == check_included(@guess)
+      end
+    end
+  end
 end
 
 Game.new
