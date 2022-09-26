8aaa667aa14ce4d25a037b06e388b523ae5ef811
franius8 <skawinskifranciszek@gmail.com>
2022-09-26 10:37:47 +0200

    Splin main function

--- sub-strings/sub_strings.rb
+++ sub-strings/sub_strings.rb
@@ -2,12 +2,17 @@
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-def substrings(string, dictionary)
-  main_string = string.downcase
-  result = Hash.new(0)
+def dictionary_check(dictionary, main_string)
+  result = {}
   dictionary.each do |word|
     result[word] = main_string.scan(word).length if main_string.include?(word)
   end
+  result
+end
+
+def substrings(string, dictionary)
+  main_string = string.downcase
+  result = dictionary_check(dictionary, main_string)
   if result == {}
     puts 'No matches found!'
   else
