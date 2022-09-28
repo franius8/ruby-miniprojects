8f815b0c194baf8efa4ef78cc90d09f948f3592f
franius8 <skawinskifranciszek@gmail.com>
2022-09-28 09:22:53 +0200

    Change to use classes and prepare to add decoding

--- caesar-cipher/cipher.rb
+++ caesar-cipher/cipher.rb
@@ -1,43 +1,68 @@
 # frozen_string_literal: true
 
-def letter?(number)
-  number.between?(65, 90) || number.between?(97, 122)
-end
 
-def outside_range?(number, char)
-  number > 122 || (char.ord.between?(65, 90) && number > 90)
-end
-
-def shift_value(number, char, shift)
-  number += shift
-  number -= 26 while outside_range?(number, char)
-  number.chr
-end
-
-def return_shifted_value(char, shift)
-  number = char.ord
-  if letter?(number)
-    shift_value(number, char, shift)
-  else
-    char
+class CaesarCipher
+  def initialize
+    loop do
+      puts 'Welcome to Caesar Cipher!'
+      puts 'Press 1 to encode or 2 to decode'
+      case gets.chomp.to_i
+      when 1
+      CipherEncode.new
+      when 2
+      decode
+      else
+      puts 'Invalid selection, try again'
+      end 
+    end
   end
 end
 
-def encode_string(string, shift)
-  string.split('').reduce([]) do |final_array, char|
-    final_array << return_shifted_value(char, shift)
+class CipherEncode
+  def initialize
+    print 'Enter the phrase to encode: '
+    @string = gets.chomp
+
+    print 'Enter the shift value: '
+    @shift = gets.chomp.to_i
+
+    @final_array = []
+    caesar_encode
+  end
+
+  def caesar_encode
+    encode_string(@string, @shift)
+    puts @final_array.join('')
+  end
+
+  def encode_string(string, shift)
+    string.split('').each do |char|
+      @final_array << return_shifted_value(char)
+    end
+  end
+
+  def return_shifted_value(char)
+    @number = char.ord
+    if letter?
+      shift_value(char)
+    else
+      char
+    end
+  end
+
+  def shift_value(char)
+    @number += @shift
+    @number -= 26 while outside_range?(char)
+    @number.chr
+  end
+
+  def letter?
+    @number.between?(65, 90) || @number.between?(97, 122)
+  end
+  
+  def outside_range?(char)
+    @number > 122 || (char.ord.between?(65, 90) && @number > 90)
   end
 end
 
-def caesar_cipher(string, shift)
-  final_array = encode_string(string, shift)
-  puts final_array.join('')
-end
-
-print 'Enter the phrase to encode: '
-string = gets.chomp
-
-print 'Enter the shift value: '
-shift = gets.chomp.to_i
-
-caesar_cipher(string, shift)
+CaesarCipher.new
