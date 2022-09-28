f1c923b2df24b7b83499df505b6a9db8b227f4d9
franius8 <skawinskifranciszek@gmail.com>
2022-09-28 09:41:12 +0200

    Add decode class, move common elements to module

--- caesar-cipher/cipher.rb
+++ caesar-cipher/cipher.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
+# Module with elements used in both decode and encode classes
+module CommonElements
+  def get_user_input
+    print "Enter the phrase to #{@mode}: "
+    @string = gets.chomp
+
+    print 'Enter the shift value: '
+    @shift = gets.chomp.to_i
+  end
+
+  def return_shifted_value
+    @number = @char.ord
+    if letter?
+      shift_value
+    else
+      char
+    end
+  end
+
+  def letter?
+    @number.between?(65, 90) || @number.between?(97, 122)
+  end
+end
+
 # Class initializing the cipher
 class CaesarCipher
   def initialize
@@ -8,7 +32,7 @@ class CaesarCipher
       puts 'Press 1 to encode or 2 to decode'
       case gets.chomp.to_i
       when 1 then CipherEncode.new
-      when 2 then decode
+      when 2 then CipherDecode.new
       else puts 'Invalid selection, try again'
       end
     end
@@ -17,49 +41,69 @@ end
 
 # Class for encoding strings
 class CipherEncode
+  include CommonElements
   def initialize
-    print 'Enter the phrase to encode: '
-    @string = gets.chomp
-
-    print 'Enter the shift value: '
-    @shift = gets.chomp.to_i
-
+    @mode = 'encode'
     @final_array = []
+
+    get_user_input
     caesar_encode
   end
 
   def caesar_encode
-    encode_string(@string, @shift)
+    encode_string
     puts @final_array.join('')
   end
 
-  def encode_string(string, _shift)
-    string.split('').each do |char|
-      @final_array << return_shifted_value(char)
+  def encode_string
+    @string.split('').each do |char|
+      @char = char
+      @final_array << return_shifted_value
     end
   end
 
-  def return_shifted_value(char)
-    @number = char.ord
-    if letter?
-      shift_value(char)
-    else
-      char
-    end
-  end
-
-  def shift_value(char)
+  def shift_value
     @number += @shift
-    @number -= 26 while outside_range?(char)
+    @number -= 26 while outside_range?
     @number.chr
   end
 
-  def letter?
-    @number.between?(65, 90) || @number.between?(97, 122)
+  def outside_range?
+    @number > 122 || (@char.ord.between?(65, 90) && @number > 90)
+  end
+end
+
+# Class for decoding the string
+class CipherDecode
+  include CommonElements
+  def initialize
+    @mode = 'decode'
+    @final_array = []
+
+    get_user_input
+    caesar_decode
   end
 
-  def outside_range?(char)
-    @number > 122 || (char.ord.between?(65, 90) && @number > 90)
+  def caesar_decode
+    decode_string
+    puts @final_array.join('')
+  end
+
+  def decode_string
+    @string.split('').each do |char|
+      @char = char
+      @final_array << return_shifted_value
+    end
+  end
+
+  def shift_value
+    @number -= @shift
+    @number += 26 while outside_range?
+    @number.chr
+  end
+
+  def outside_range?
+    @number < 65 || (@char.ord.between?(97, 122) && @number < 97)
   end
 end
 
