c58a2c8b55de456ab0dfb6d95d02e2b58d03c570
franius8 <skawinskifranciszek@gmail.com>
2022-10-12 15:51:51 +0200

    Add subtract tests

--- rspec_test/spec/calculator_spec.rb
+++ rspec_test/spec/calculator_spec.rb
@@ -28,5 +28,20 @@ describe Calculator do
             calculator = Calculator.new
             expect(calculator.subtract(3,2)).to eql(1)
         end
+
+        it 'returns the subtraction of more than two numbers' do
+            calculator = Calculator.new
+            expect(calculator.subtract(7,2,1)).to eql(4)
+        end
+
+        it 'returns the number if only one argument is given' do
+            calculator = Calculator.new
+            expect(calculator.subtract(1)).to eql(1)
+        end
+
+        it 'returns nil with no arguments' do
+            calculator = Calculator.new
+            expect(calculator.subtract).to eql(nil)
+        end
     end
 end
