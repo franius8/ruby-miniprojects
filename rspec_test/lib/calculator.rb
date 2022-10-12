1ece8cae060737722959c7e6da648cebe9f0c600
franius8 <skawinskifranciszek@gmail.com>
2022-10-12 15:52:12 +0200

    Expand add method, add subtract method

--- rspec_test/lib/calculator.rb
+++ rspec_test/lib/calculator.rb
@@ -1,5 +1,17 @@
 class Calculator
-    def add(a,b)
-        a+b
+    def add(*numbers)
+        return nil if numbers.empty?
+        numbers.reduce(0) {|result, number| number += result}
+    end
+
+    def subtract(*numbers)
+        return nil if numbers.empty?
+        numbers.reduce(nil) do |result, number|
+            if result == nil
+                result = number
+            else
+                result -= number
+            end
+        end
     end
 end
