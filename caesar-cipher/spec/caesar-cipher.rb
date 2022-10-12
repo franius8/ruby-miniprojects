c802baf3aa2590717c0c5a226d3955fa29a03745
franius8 <skawinskifranciszek@gmail.com>
2022-10-12 21:05:54 +0200

    Add basic test (WIP)

--- caesar-cipher/spec/caesar-cipher.rb
+++ caesar-cipher/spec/caesar-cipher.rb
@@ -1,1 +1,13 @@
 require './lib/cipher'
+
+describe CipherEncode do
+    before(:example) do
+        @shift = 2
+        @string = 'a'
+      end
+    describe '#shift_value' do
+        it 'shifts the letter by shift factor'
+        encode = CipherEncode.new
+    end
+
+end
