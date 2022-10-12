361e97e251441bd331bed9d7d51bdccb13795853
franius8 <skawinskifranciszek@gmail.com>
2022-10-12 15:57:04 +0200

    Code refactoring

--- rspec_test/lib/calculator.rb
+++ rspec_test/lib/calculator.rb
@@ -1,17 +1,22 @@
-class Calculator
-    def add(*numbers)
-        return nil if numbers.empty?
-        numbers.reduce(0) {|result, number| number += result}
-    end
+# frozen_string_literal: true
 
-    def subtract(*numbers)
-        return nil if numbers.empty?
-        numbers.reduce(nil) do |result, number|
-            if result == nil
-                result = number
-            else
-                result -= number
-            end
-        end
+# Performs basic arithmetical operations
+class Calculator
+  def add(*numbers)
+    return nil if numbers.empty?
+
+    numbers.reduce(0) { |result, number| result += number }
+  end
+
+  def subtract(*numbers)
+    return nil if numbers.empty?
+
+    numbers.reduce(nil) do |result, number|
+      if result.nil?
+        result = number
+      else
+        result -= number
+      end
     end
+  end
 end
--- rspec_test/spec/calculator_spec.rb
+++ rspec_test/spec/calculator_spec.rb
@@ -1,47 +1,49 @@
+# frozen_string_literal: true
+
 require './lib/calculator'
 
 describe Calculator do
-    describe '#add' do
-        it 'returns the sum of two numbers' do
-            calculator = Calculator.new
-            expect(calculator.add(5,2)).to eql(7)
-        end
-
-        it 'returns the sum of more than two numbers' do
-            calculator = Calculator.new
-            expect(calculator.add(5,2, 6, 7)).to eql(20)
-        end
-
-        it 'returns the number if only one argument is given' do
-            calculator = Calculator.new
-            expect(calculator.add(1)).to eql(1)
-        end
-
-        it 'returns nil with no arguments' do
-            calculator = Calculator.new
-            expect(calculator.add).to eql(nil)
-        end
+  describe '#add' do
+    it 'returns the sum of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.add(5, 2)).to eql(7)
     end
 
-    describe '#subtract' do
-        it 'returns the subtraction of two numbers' do
-            calculator = Calculator.new
-            expect(calculator.subtract(3,2)).to eql(1)
-        end
-
-        it 'returns the subtraction of more than two numbers' do
-            calculator = Calculator.new
-            expect(calculator.subtract(7,2,1)).to eql(4)
-        end
-
-        it 'returns the number if only one argument is given' do
-            calculator = Calculator.new
-            expect(calculator.subtract(1)).to eql(1)
-        end
-
-        it 'returns nil with no arguments' do
-            calculator = Calculator.new
-            expect(calculator.subtract).to eql(nil)
-        end
+    it 'returns the sum of more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.add(5, 2, 6, 7)).to eql(20)
     end
+
+    it 'returns the number if only one argument is given' do
+      calculator = Calculator.new
+      expect(calculator.add(1)).to eql(1)
+    end
+
+    it 'returns nil with no arguments' do
+      calculator = Calculator.new
+      expect(calculator.add).to eql(nil)
+    end
+  end
+
+  describe '#subtract' do
+    it 'returns the subtraction of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.subtract(3, 2)).to eql(1)
+    end
+
+    it 'returns the subtraction of more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.subtract(7, 2, 1)).to eql(4)
+    end
+
+    it 'returns the number if only one argument is given' do
+      calculator = Calculator.new
+      expect(calculator.subtract(1)).to eql(1)
+    end
+
+    it 'returns nil with no arguments' do
+      calculator = Calculator.new
+      expect(calculator.subtract).to eql(nil)
+    end
+  end
 end
