713c1780d32abeec4461554b789ffac83f71bad9
franius8 <skawinskifranciszek@gmail.com>
2022-10-12 15:45:50 +0200

    Add new tests, fix typo

--- rspec_test/spec/calculator_spec.rb
+++ rspec_test/spec/calculator_spec.rb
@@ -2,9 +2,31 @@ require './lib/calculator'
 
 describe Calculator do
     describe '#add' do
-        it 'retourns the sum of two number' do
+        it 'returns the sum of two numbers' do
             calculator = Calculator.new
             expect(calculator.add(5,2)).to eql(7)
+        end
+
+        it 'returns the sum of more than two numbers' do
+            calculator = Calculator.new
+            expect(calculator.add(5,2, 6, 7)).to eql(20)
+        end
+
+        it 'returns the number if only one argument is given' do
+            calculator = Calculator.new
+            expect(calculator.add(1)).to eql(1)
+        end
+
+        it 'returns nil with no arguments' do
+            calculator = Calculator.new
+            expect(calculator.add).to eql(nil)
+        end
+    end
+
+    describe '#subtract' do
+        it 'returns the subtraction of two numbers' do
+            calculator = Calculator.new
+            expect(calculator.subtract(3,2)).to eql(1)
         end
     end
 end
